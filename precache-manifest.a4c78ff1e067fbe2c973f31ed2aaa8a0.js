423eadc879114bb3b0bbcefdc51eafafded450da
Anna Buczek <aania.buczek@gmail.com>
2020-07-01 17:56:57 +0200

    Updates

---
+++ precache-manifest.a4c78ff1e067fbe2c973f31ed2aaa8a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f15b700fba244b2ccaaac6a809b89307",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "1d86c6d2f4925513d13f",
+    "url": "/calculator/static/css/main.3a00ca6b.chunk.css"
+  },
+  {
+    "revision": "39cb557ef04cdf4f503a",
+    "url": "/calculator/static/js/2.def17f36.chunk.js"
+  },
+  {
+    "revision": "01b52e8cfa161811ba4cbd78f485a21f",
+    "url": "/calculator/static/js/2.def17f36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d86c6d2f4925513d13f",
+    "url": "/calculator/static/js/main.b9f94270.chunk.js"
+  },
+  {
+    "revision": "ee4a148f714da062db4f",
+    "url": "/calculator/static/js/runtime-main.f0d0de70.js"
+  }
+]);
